abbe72d6d8f16f0af8d2c8b1fe6f17f9fafb15f7
Sijia(Konic) Ge <sijiage007@gmail.com>
2022-05-29 16:42:42 -0600

    solve the bugs for loop and add another implementation

--- pythonDS/chapter5 search-order.py
+++ pythonDS/chapter5 search-order.py
@@ -61,28 +61,33 @@ def hash_words(word, size):
 implement the dict() from scatch,
 the original hash function is using the key % the hash table size
 and the rehash function is the original hashvalue +1 % hash tabel size
-
+the slot can be  1) open  2) there exits the same key but old value 3) has been taken by other key
+so that, get the second hashvalue by hashvalue +1 and mod the size of hashtable 
 '''
 
 class Hashtable:
     def __init__(self,size):
         self.size=size
+        # build two lists to store the key and value
         self.slots=[None]*self.size
         self.data= self.size *[None]
 
     def put(self,key,data):
-        hashvalue= self.hashfunc(key)
-        print(hashvalue)
-        if self.slots[hashvalue]==None:
+        hashvalue= self.hashfunc(key)# get the initial hashvalue
+        # print(hashvalue)
+        if self.slots[hashvalue]==None:  # if the slot not be taken, set the key-value pair
             self.slots[hashvalue]=key
             self.data[hashvalue]=data
-        elif self.slots[hashvalue]==key:
+        elif self.slots[hashvalue]==key:    # if there exists an old value for that key, update the data
             self.data[hashvalue]=data
-        else:
-            nextvalue= self.rehash(hashvalue)
+        else:  # if there is no key in the slots and the initial slot has been taken
+            nextvalue= self.rehash(hashvalue)  # get the second hash value, remember get the second hash value first
             # print(nextvalue)
+            # one justify, whether the second value meet the requirement; until the slot meet the requirement
             while self.slots[nextvalue] != None and self.slots[nextvalue] !=key:
                 nextvalue=self.rehash(nextvalue)
+
+            # meet the requirement  two situations:  1) the slot is open   2) there exists the old value for the key.
             if self.slots[nextvalue]==None:
                 self.slots[nextvalue]=key
                 self.data[nextvalue]=data
@@ -93,22 +98,38 @@ class Hashtable:
 
     def rehash(self,value):
         # notice here is not use the old value get the new value directly, that will not change the value, add 1
-        return (value+1)%self.size
+        return (value+1) % self.size
     def get(self,key):
-        data=None # default return value
+        data=None # default return value, remember it has to be a return value.
         stop=False
-        # found=False
-        hashvalue=self.hashfunc(key)
-        startposition=hashvalue
-        while self.slots[hashvalue] != None and not stop:
-            if self.slots[hashvalue]!= key:    # notice, even if the current slot is not none, if might be taken up by the other value
-                data= self.data[hashvalue]
+        found=False
+        startslot=self.hashfunc(key)
+        position=startslot   # important, justify that the hashvalue has returned to the initial hashvalue
+        # , stop the loop
+        while self.slots[position] is not None and not stop:  # because it's search, so it has to be equivalent to the key,
+            # # rather than open, notice the loop variable should be the same,  !false: hashvalue, nextvalue
+            # print(position)
+            if self.slots[position]== key:    # notice, even if the current slot is not none, if might be taken up by the other value
+                data= self.data[position]
                 break
                 # found=True
-            else:
-                nextvalue= self.rehash(hashvalue)
-                if nextvalue == startposition:
+            else: # if not key, continue getting the second hashvalue
+
+                position= self.rehash(position)
+                # print(nextvalue)
+                if position== startslot:  # if the hashvalue = initial hashvalue, it means has one round, stop the loop
                     stop=True
+        #     '''
+        #     the other implementation for the loop
+        #     '''
+        # while self.slots[position] != key:
+        #     if self.slots[position] == startslot:
+        #         break
+        #     position=self.rehash(position)
+        # # the loop ended, either get the initial key, or not found in the end,
+        # # if get the key, update the data from none to the value
+        # if self.slots[position]== key:
+        #     data= self.data[position]
 
         return data
 
@@ -117,6 +138,15 @@ class Hashtable:
 
     def __setitem__(self, key, value):
         return self.put(key,value)
+
+
+# class Hashtable:
+#     def __init__(self, size):
+#         self.size = size
+#         self.slots = self.size * [None]
+#         self.data = self.size * [None]
+
+
 if __name__=='__main__':
     # testlist = [0, 1 ]
     # print(binary_search_rec(testlist, 0))
@@ -135,9 +165,9 @@ if __name__=='__main__':
     H[20] = 'CHICKEN'
     print(H.slots)
     print(H.data)
-    H[20]
-    H[17]
+    print(H[20])
+    print(H[17])
     H[20] = 'DUCK'
     print(H[20])
-    print(H.data)
+    # print(H.data)
     # print(sequential_search([1,2,5,7,9],2))
