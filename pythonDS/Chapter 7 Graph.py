a22c2ebe6dc9f4d3a1ff2c8a87f6ee477183fd9b
Sijia(Konic) Ge <94318777+Konic-NLP@users.noreply.github.com>
2022-08-31 00:09:52 -0600

    add typological sort

--- pythonDS/Chapter 7 Graph.py
+++ pythonDS/Chapter 7 Graph.py
@@ -301,6 +301,76 @@ def Dijkstra(aGraph:Graph,start):
                 pq.decrease(neighbor,newdist)  # rearrange the location of the neighbor in the pq
 
 
+                
+                
+'''
+Typological sort, DFS iterate all vertices, when one node and its neighbors all are visited, add the current node into the begining of the list
+'''
+
+# A class to represent a graph object
+class Graph:
+    def __init__(self, edges, n):
+ 
+        # A list of lists to represent an adjacency list
+        self.adjList = [[] for _ in range(n)]
+ 
+        # add edges to the directed graph
+        for (src, dest) in edges:
+ 
+            # add an edge from source to destination
+            self.adjList[src].append(dest)
+ 
+ 
+# Perform DFS on the graph and set the departure time of all
+# vertices of the graph
+def DFS(graph, v, discovered, departure, time):
+ 
+    # mark the current node as discovered
+    discovered[v] = True
+ 
+    # set the arrival time of vertex `v`
+    time = time + 1
+ 
+    # do for every edge (v, u)
+    for u in graph.adjList[v]:
+        # if `u` is not yet discovered
+        if not discovered[u]:
+            time = DFS(graph, u, discovered, departure, time)
+ 
+    # ready to backtrack
+    # set departure time of vertex `v`
+    departure[time] = v
+    time = time + 1
+ 
+    return time
+ 
+ 
+# Function to perform a topological sort on a given DAG
+def doTopologicalSort(graph, n):
+ 
+    # departure[] stores the vertex number using departure time as an index
+    departure = [-1] * 2 * n
+ 
+    ''' If we had done it the other way around, i.e., fill the array
+        with departure time using vertex number as an index, we would
+        need to sort it later '''
+ 
+    # to keep track of whether a vertex is discovered or not
+    discovered = [False] * n
+    time = 0
+ 
+    # perform DFS on all undiscovered vertices
+    for i in range(n):
+        if not discovered[i]:
+            time = DFS(graph, i, discovered, departure, time)
+ 
+    # Print the vertices in order of their decreasing
+    # departure time in DFS, i.e., in topological order
+    for i in reversed(range(2*n)):
+        if departure[i] != -1:
+            print(departure[i], end=' ')
+ 
+
 '''Prime'''
 '''each time select a safe edge that the other node not in the tree add to the tree'''
 def Prim(G:Graph,start):
